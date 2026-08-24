b6f813dff8d6a357a8d8367973d94513a0936259
Godson Nudzor <godsonnudzor@gmail.com>
2026-08-24 10:28:04 +0000

    Designing Employees Dashboard

--- frontend/src/Pages/EmployeeDashboard.jsx
+++ frontend/src/Pages/EmployeeDashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const EmployeeDashboard = () => {
   return (
-    <div>
+    <div className='text-3xl text-teal-500 font-bold text-center mt-20'>
       Welcome to Employee Dashboard
     </div>
   )
