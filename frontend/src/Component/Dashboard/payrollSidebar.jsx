68e8e5037bf1b1c50bd7796e4f53b6d0dd3bc6f5
Godson Nudzor <godsonnudzor@gmail.com>
2026-09-08 11:37:27 +0000

    add design 4

--- frontend/src/Component/Dashboard/payrollSidebar.jsx
+++ frontend/src/Component/Dashboard/payrollSidebar.jsx
@@ -10,19 +10,19 @@ const payrollSidebar = () => {
            <h3 className='text-2xl text-center font-pacific'>Payroll </h3>
         </div>
         <div className='px-4'>
-            <NavLink to="/payroll" className={({ isActive }) => isActive ? 'bg-teal-600 text-white rounded-md px-2 py-1 flex items-center space-x-2' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-2 py-1 flex items-center space-x-2'}>
+            <NavLink to="/payroll" className={({ isActive }) => isActive ? 'bg-teal-600 text-cadetBlue rounded-md px-2 py-1 flex items-center space-x-2' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-2 py-1 flex items-center space-x-2'}>
                 <FaTachometerAlt /> <span>Payroll</span>
             </NavLink>
-            <NavLink to="/employees" className={({ isActive }) => isActive ? 'bg-teal-600 text-white rounded-md px-2 py-1 flex items-center space-x-2' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-2 py-1 flex items-center space-x-2'}>
+            <NavLink to="/employees" className={({ isActive }) => isActive ? 'bg-teal-600 text-cadetBlue rounded-md px-2 py-1 flex items-center space-x-2' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-2 py-1 flex items-center space-x-2'}>
                 <FaUsers /> <span>Employees List</span>
             </NavLink>
-             <NavLink to="/departments" className={({ isActive }) => isActive ? 'bg-teal-600 text-white rounded-md px-2 py-1 flex items-center space-x-2' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-2 py-1 flex items-center space-x-2'}>
+             <NavLink to="/departments" className={({ isActive }) => isActive ? 'bg-teal-600 text-cadetBlue rounded-md px-2 py-1 flex items-center space-x-2' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-2 py-1 flex items-center space-x-2'}>
                 <FaBuilding /> <span>Departments</span>
             </NavLink>
-            <NavLink to="/leave" className={({ isActive }) => isActive ? 'bg-teal-600 text-white rounded-md px-2 py-1 flex items-center space-x-2' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-2 py-1 flex items-center space-x-2'}>
+            <NavLink to="/leave" className={({ isActive }) => isActive ? 'bg-teal-600 text-cadetBlue rounded-md px-2 py-1 flex items-center space-x-2' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-2 py-1 flex items-center space-x-2'}>
                 <FaCalendar /> <span>Leave</span>
             </NavLink>
-            <NavLink to="/salaries" className={({ isActive }) => isActive ? 'bg-teal-600 text-white rounded-md px-2 py-1 flex items-center space-x-2' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-2 py-1 flex items-center space-x-2'}>
+            <NavLink to="/salaries" className={({ isActive }) => isActive ? 'bg-teal-600 text-cadetBlue rounded-md px-2 py-1 flex items-center space-x-2' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-2 py-1 flex items-center space-x-2'}>
                 <FaMoneyBill /> <span>Salaries</span>
             </NavLink>
         </div>
