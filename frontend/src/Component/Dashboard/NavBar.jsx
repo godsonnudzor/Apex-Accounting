07a85f6c349aa09e9456d3574ca82254a71eb0c3
Godson Nudzor <godsonnudzor@gmail.com>
2026-09-08 13:50:35 +0000

    constructing the navbar

---
+++ frontend/src/Component/Dashboard/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useAuth } from '../../context/authContext';
+
+const navBar = () => {
+  const { user } = useAuth();
+
+  return (
+    <div className='flex justify-between h-12 items-center p-4 bg-teal-500 text-white'>
+        <p>Welcome, {user?.name || "Employee"}</p>
+        <button className="bg-teal-500 text-white px-4 py-2 rounded">Logout</button>
+      
+    </div>
+  )
+}
+
+export default navBar
--- frontend/src/Pages/PayrollDashboard.jsx
+++ frontend/src/Pages/PayrollDashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import PayrollDashboardSidebar from '../Component/Dashboard/payrollSidebar'
+import NavBar from '../Component/Dashboard/navBar'
 
 const PayrollDashboard = props => {
   return (
-    <div>
+    <div className='flex'>
         <PayrollDashboardSidebar />
+        <div className='flex-1 ml-64 bg-gray-100 h-screen'>
+          <NavBar />
+        </div>
       
     </div>
   )
