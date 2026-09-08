90d5937e1af8ea454233cabbc8ce684a9ddf5471
Godson Nudzor <godsonnudzor@gmail.com>
2026-09-08 11:21:11 +0000

    Adding design 1 to Payroll

--- frontend/src/Component/Dashboard/payrollSidebar.jsx
+++ frontend/src/Component/Dashboard/payrollSidebar.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import { FaTachometerAlt } from 'react-icons/fa'
+import { FaTachometerAlt, FaMoneyBill, FaUsers,FaBuilding, FaCalendar } from 'react-icons/fa'
+
 
 const payrollSidebar = () => {
   return (
-    <div>
-        <div className='sidebarWrapper'>
+    <div className='bg-gray-800 text-white h-screen fixed left-0 top-0 bottom-0 space-y-2 w-64 '>
+        <div>
            <h3>Payroll Sidebar</h3>
         </div>
         <div>
-            <NavLink to="/PayrollDashboard" className={({ isActive }) => (isActive ? 'active' : '')}>
+            <NavLink to="/payroll">
                 <FaTachometerAlt /> <span>Payroll</span>
+            </NavLink>
+            <NavLink to="/employees">
+                <FaUsers /> <span>Employees List</span>
+            </NavLink>
+             <NavLink to="/departments">
+                <FaBuilding /> <span>Departments</span>
+            </NavLink>
+            <NavLink to="/leave">
+                <FaCalendar /> <span>Leave</span>
+            </NavLink>
+            <NavLink to="/salaries">
+                <FaMoneyBill /> <span>Salaries</span>
             </NavLink>
         </div>
       
