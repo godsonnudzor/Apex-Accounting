0859891da63b03e744b549d79d2bbaabb8e85fc1
Godson Nudzor <godsonnudzor@gmail.com>
2026-09-08 13:52:23 +0000

    narbar bg

--- frontend/src/Component/Dashboard/NavBar.jsx
+++ frontend/src/Component/Dashboard/NavBar.jsx
@@ -5,7 +5,7 @@ const navBar = () => {
   const { user } = useAuth();
 
   return (
-    <div className='flex justify-between h-12 items-center p-4 bg-teal-500 text-white'>
+    <div className='flex justify-between h-12 items-center p-4 bg-teal-600 text-white'>
         <p>Welcome, {user?.name || "Employee"}</p>
         <button className="bg-teal-500 text-white px-4 py-2 rounded">Logout</button>
       
