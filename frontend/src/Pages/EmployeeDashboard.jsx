4243c9c34a8841f5381ec7c361e335faa96c0ebe
Godson Nudzor <godsonnudzor@gmail.com>
2026-09-08 12:23:03 +0000

    add report permission to employees dashboard

--- frontend/src/Pages/EmployeeDashboard.jsx
+++ frontend/src/Pages/EmployeeDashboard.jsx
@@ -12,6 +12,8 @@ const EmployeeDashboard = () => {
         {hasPermission("bills") ? <Link className="rounded bg-white px-5 py-3 shadow" to="/bill">Bills</Link> : null}
         {hasPermission("writeCheque") ? <Link className="rounded bg-white px-5 py-3 shadow" to="/write-cheque">Write cheque</Link> : null}
         {hasPermission("payroll") ? <Link className="rounded bg-white px-5 py-3 shadow" to="/PayrollDashboard">Payroll</Link> : null}
+        {hasPermission("Reports") ? <Link className="rounded bg-white px-5 py-3 shadow" to="/Reports">Reports</Link> : null}
+
       </div>
     </div>
   );
