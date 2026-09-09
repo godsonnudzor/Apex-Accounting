71c13151eebeccfe0f01e8aba7e30484558f6c11
Godson Nudzor <godsonnudzor@gmail.com>
2026-09-09 14:55:25 +0000

    Adding title to payrolSummery

--- frontend/src/Component/Dashboard/payrollSummary.jsx
+++ frontend/src/Component/Dashboard/payrollSummary.jsx
@@ -11,11 +11,14 @@ const payrollSummary = () => {
             <SummaryCards icon={FaBuilding} text="Total Departments" number={4} colour="bg-yellow-600" />
             <SummaryCards icon={FaMoneyBillAlt} text="Total Salaries" number={100000} colour="bg-red-600" />
         </div>
+        <div className="mt-12">
+          <h3 className="text-center text-xl font-semibold">Leave Summary</h3>
         <div className="grid grid-cols-1 md:grid-cols-2  gap-4 mt-6">
             <SummaryCards icon={FaFileAlt} text="Leave Applied" number={5} colour="bg-teal-600" />
             <SummaryCards icon={FaRegCheckCircle} text="Leave Approved" number={3} colour="bg-green-200" />
             <SummaryCards icon={FaHourglassHalf} text="Leave Pending" number={1} colour="bg-yellow-600" />
             <SummaryCards icon={FaHourglassHalf} text="Leave Rejected" number={1} colour="bg-red-600" />
+        </div>
         </div>
     </div>
   )
