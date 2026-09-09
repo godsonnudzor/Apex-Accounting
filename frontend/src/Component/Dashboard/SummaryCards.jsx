49c071e1ab9c77254f6ee9ddcfefb4a94d5ca9de
Godson Nudzor <godsonnudzor@gmail.com>
2026-09-09 14:26:21 +0000

    Designing of Sumary cards

--- frontend/src/Component/Dashboard/SummaryCards.jsx
+++ frontend/src/Component/Dashboard/SummaryCards.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const SummaryCards = ({ icon, text, number }) => {
+const SummaryCards = ({ icon, text, number, colour }) => {
   const Icon = icon
 
   return (
     <div className="bg-white rounded flex ">
-        <div className="text-3xl bg-teal-600 p-4  flex items-center justify-center">
+        <div className={`text-3xl ${colour || 'bg-teal-600'} p-4  flex items-center justify-center`}>
             <Icon />
         </div>
         <div className="pl-4 py-1">
             <p className="text-lg font-semibold">{text}</p>
-            <p className="text-xl font-bold">${number.toLocaleString()}</p>
+            <p className="text-xl font-bold">${number}</p>
         </div>
       
     </div>
--- frontend/src/Component/Dashboard/payrollSummary.jsx
+++ frontend/src/Component/Dashboard/payrollSummary.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import SummaryCards from './SummaryCards'
-import { FaUsers } from 'react-icons/fa'
+import { FaUsers, FaBuilding, FaMoneyBillAlt } from 'react-icons/fa'
 
 const payrollSummary = () => {
   return (
     <div className="p-6">
         <h3 className="text-2xl font-bold ">Payroll Overview</h3>
         <div className="grid grid-cols-1 md:grid-cols-3  gap-4 mt-6">
-            <SummaryCards icon={FaUsers} text="Total Employees" number={100} />
+            <SummaryCards icon={FaUsers} text="Total Employees" number={13} colour="bg-blue-600" />
+            <SummaryCards icon={FaBuilding} text="Total Departments" number={4} colour="bg-teal-600" />
+            <SummaryCards icon={FaMoneyBillAlt} text="Total Salaries" number={100000} colour="bg-green-600" />
         </div>
     </div>
   )
