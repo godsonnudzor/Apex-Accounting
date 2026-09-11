c45425d70a607946ca256af7589f6a8451d7f191
Godson Nudzor <godsonnudzor@gmail.com>
2026-09-11 12:37:20 +0000

    Designing departs

--- frontend/src/Component/Department/Add.jsx
+++ frontend/src/Component/Department/Add.jsx
@@ -2,8 +2,34 @@ import React from 'react'
 
 const Add = () => {
   return (
-    <div>
-      <h3 className='text-2xl font-bold'>Add New Department</h3>
+    <div className='p-5'>
+      <div>
+        <h3 className='text-2xl font-bold'>Add New Department</h3>
+        <form>
+          <div className='flex flex-col gap-4 mt-4'>
+            <Label htmlFor="dep_name"
+              className='block mb-1'>
+              Department Name
+              </Label>
+            <input type="text" 
+            id="dep_name" 
+            name="dep_name" 
+            placeholder="Enter dept name" 
+            className='px-4 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full' />
+          </div>
+          <div className='flex flex-col gap-4 mt-4'>
+            <Label htmlFor="description"
+              className='block mb-1'>
+              Description
+            </Label>
+            <textarea 
+              name="description"
+              placeholder=" description"
+              className='px-4 py-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full' />
+          </div>
+        </form>
+        <button className='px-4 py-2 bg-teal-500 text-white rounded hover:bg-teal-600 mt-4'>Add Department</button>
+      </div> 
     </div>
   )
 }
--- frontend/src/Component/Department/Department.jsx
+++ frontend/src/Component/Department/Department.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 
 const Department = () => {
   return (
-    <div>
+    <div className='p-5'>
       <div className='text-center'>
         <h3 className='text-2xl font-bold'>Manage Departments</h3>
       </div>
       <div className='flex justify-between items-center p-4'>
-        <input type="text" placeholder="Search by departments Name..." className='px-4 py-0.5 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500' />
+        <input type="text" placeholder="Search by department Name" className='px-4 py-0.5 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500' />
         <Link to="/department/add" className='px-4 py-1 bg-teal-500 text-white rounded hover:bg-teal-600'>Add New Department</Link>
 
       </div>
