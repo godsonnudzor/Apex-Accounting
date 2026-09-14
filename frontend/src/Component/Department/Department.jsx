27d7c922c778a140c19b06f5bfa898cf220a0734
Godson Nudzor <godsonnudzor@gmail.com>
2026-09-14 10:42:45 +0000

    Designing dept

--- frontend/src/Component/Department/Department.jsx
+++ frontend/src/Component/Department/Department.jsx
@@ -132,7 +132,7 @@ const Department = () => {
                   <input
                     value={editName}
                     onChange={(event) => setEditName(event.target.value)}
-                    className="block w-full mb-2 px-3 py-2 border rounded"
+                    className=" w-full mb-2 px-3 py-2 border rounded"
                   />
 
                   <textarea
@@ -140,7 +140,7 @@ const Department = () => {
                     onChange={(event) =>
                       setEditDescription(event.target.value)
                     }
-                    className="block w-full mb-2 px-3 py-2 border rounded"
+                    className=" w-full mb-2 px-3 py-2 border rounded"
                   />
 
                   <button
