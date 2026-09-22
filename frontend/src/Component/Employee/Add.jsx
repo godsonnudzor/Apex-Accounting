0e1f90d7e4f145eaa2042ec8c54a25f7180fe3b5
Godson Nudzor <godsonnudzor@gmail.com>
2026-09-22 10:45:50 +0000

    adding employee to role and maintaining male and female as sex

--- frontend/src/Component/Employee/Add.jsx
+++ frontend/src/Component/Employee/Add.jsx
@@ -256,8 +256,7 @@ const Add = () => {
                     <option value="">Select sex</option>
                     <option value="female">Female</option>
                     <option value="male">Male</option>
-                    <option value="other">Other</option>
-                    <option value="prefer_not_to_say">Prefer not to say</option>
+                    
                   </select>
                 </div>
 
@@ -275,6 +274,8 @@ const Add = () => {
                     <option value="admin">Admin</option>
                     <option value="user">User</option>
                     <option value="public">Public</option>
+                     <option value="employee">Employee</option>
+                    
                   </select>
                 </div>
               </div>
